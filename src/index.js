1bd9d6320b1105ae7606c8cb370ba71bbcdc1d5c
Paweł Piskorz <pawel@arabel.la>
2017-08-02 15:26:16 +0200

    ARD-460 Fix modalReducer export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 /* @flow */
 
 export { default as ReduxModal } from './ReduxModal';
+export { default as modalReducer } from './reducer';
 
 export * from './actions';
-export * from './reducer';
 export * from './selectors';
