cfa372c0f1ca1c59e98d4eb1a491ced82b558899
Paweł Piskorz <pawel@arabel.la>
2017-08-02 13:24:41 +0200

    ARD-460 Update configs

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,35 @@
-import { configure } from '@kadira/storybook';
+import React from 'react';
+import { configure, addDecorator } from '@kadira/storybook';
+import { createStore } from 'redux';
+import { combineReducers } from 'redux-immutable';
+import { Provider } from 'react-redux';
+import { fromJS } from 'immutable';
+import modalReducer from '../src/reducer';
 
-// stories loader
-const req = require.context('../stories', true, /.js$/);
+const reducers = {
+  modal: modalReducer,
+};
+const initialState = fromJS({
+  modal: {
+    openedModalName: 'TestModal',
+  },
+});
+
+const reducer = combineReducers(reducers);
+const store = createStore(
+  reducer,
+  initialState,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+addDecorator((story) => (
+  <Provider store={store}>
+    {story()}
+  </Provider>
+));
+
+// Load stories
+const req = require.context('../src/', true, /\.stories\.js?$/);
 function loadStories() {
   req.keys().forEach((filename) => req(filename));
 }
