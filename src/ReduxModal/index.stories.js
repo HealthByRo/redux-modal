02fa6cfccd7050833bcc8eb750dc8da2268d9df9
Andrzej Dybionka <andrzej@arabel.la>
2018-03-15 15:31:48 +0100

    Update node modules

--- src/ReduxModal/index.stories.js
+++ src/ReduxModal/index.stories.js
@@ -2,21 +2,18 @@
 
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
-import { decorateAction } from '@kadira/storybook-addon-actions';
 import ReduxModal from './';
 
-const TestModal= () => <p>Example modal content</p>
+const TestModal = () => <p>Example modal content</p>;
 
 storiesOf('ReduxModal', module)
-  .add('Normal modal', () => {
-    return (
-      <div>
-        <button>dupa</button>
-        <ReduxModal
-          name="TestModal"
-          component={TestModal}
-          contentLabel="Example modal"
-        />
-      </div>
-    );
-  })
+.add('Normal modal', () => (
+  <div>
+    <button>dupa</button>
+    <ReduxModal
+      name="TestModal"
+      component={TestModal}
+      contentLabel="Example modal"
+    />
+  </div>
+));
